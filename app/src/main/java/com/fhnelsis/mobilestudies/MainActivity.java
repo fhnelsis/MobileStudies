bdea29c495ca79f88b8c70601f323da5916d56fe
Francisco Nelsis <fhnelsis@outlook.com>
2015-06-29 16:39:38 -0300

    Initial commit

--- app/src/main/java/com/fhnelsis/mobilestudies/MainActivity.java
+++ app/src/main/java/com/fhnelsis/mobilestudies/MainActivity.java
@@ -1,17 +1,19 @@
 package com.fhnelsis.mobilestudies;
 
+import android.app.Activity;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.TextView;
 
-
-public class MainActivity extends ActionBarActivity {
+public class MainActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        TextView tx = (TextView) findViewById(R.id.texto1);
     }
 
     @Override
